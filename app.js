974439d437f551b36f51a7ac601a20aef4318557
cardvark <endaction@gmail.com>
2015-07-02 22:47:31 -0700

    Looks done.

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+	$('.inspiration-getter').submit( function(event){
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tag = $(this).find("input[name='answerers']").val();
+		var period = $("#param-period option:selected").text(); 
+		getAnswerers(tag, period);
+	});
+
+
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -20,7 +30,56 @@ var questionResultTemplate =
 			'<dt>Viewed</dt>' +
 				'<dd class="viewed">{{qViewCount}}</dd>' +
 			'<dt>Asker</dt>' +
-				'<dd class="asker"></dd>';
+				'<dd class="asker">' + 
+					'<p>Name: <a target="_blank" href="http://stackoverflow.com/users/{{qUserId}}" >{{qDisplayName}}</a>' +
+					'</p>' +
+					'<p>Reputation: {{qRep}}</p>'
+				'</dd>';
+
+var answererResultTemplate = 
+				'<dt>User:</dt>' +
+					'<dd>' +
+						'<p>' +
+							'<img src="{{aIcon}}" alt="userImage" class="userIcon">' +
+								'Name: <a href="{{aLink}}">{{aName}}</a>' +
+						'</p>' +
+						'<p>' +
+							'User Type: {{aType}}' +
+						'</p>' +
+					'</dd>' +
+				'<dt>Stats:</dt>' +
+					'<dd>' +
+						'<p>' +
+							'Reputation: {{aRep}}' +
+						'</p>' +
+						'<p>' +
+							'Post Count: {{aCount}}' +
+						'</p>' +
+						'<p>' +
+							'Score: {{aScore}}' +
+						'</p>' +
+					'</dd>';
+
+var showAnswerer = function(answerer) {
+	var result = $('.templates .topAnswerer').clone();
+	
+	var data = {
+		aIcon: answerer.user.profile_image,
+		aLink: answerer.user.link,
+		aName: answerer.user.display_name,
+		aType: answerer.user.user_type,
+		aRep: answerer.user.reputation,
+		aCount: answerer.post_count,
+		aScore: answerer.score
+	}
+	console.log(data);
+
+	var newHtml = Mustache.to_html(answererResultTemplate, data);
+
+	result.html(newHtml);
+
+	return result;
+};
 
 var showQuestion = function(question) {
 	
@@ -31,57 +90,18 @@ var showQuestion = function(question) {
 		qLink: question.link,
 		qTitle: question.title,
 		qDate: new Date(1000*question.creation_date).toString(),
-		qViewCount: question.view_count
+		qViewCount: question.view_count,
+		qUserId: question.owner.user_id,
+		qDisplayName: question.owner.display_name,
+		qRep: question.owner.reputation
 	};
 
 	var newHtml = Mustache.to_html(questionResultTemplate, data);
 
 	result.html(newHtml);
 
-	// set some properties related to asker
-	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-													question.owner.display_name +
-												'</a>' +
-							'</p>' +
- 							'<p>Reputation: ' + question.owner.reputation + '</p>'
-	);
-
 	return result;
 };
-
-/*var showQuestion = function(question) {
-	
-	// clone our result template code
-	var result = $('.templates .question').clone();
-	
-	// Set the question properties in result
-	var questionElem = result.find('.question-text a');
-	questionElem.attr('href', question.link);
-	questionElem.text(question.title);
-
-	// set the date asked property in result
-	var asked = result.find('.asked-date');
-	var date = new Date(1000*question.creation_date);
-	asked.text(date.toString());
-
-	// set the #views for question property in result
-	var viewed = result.find('.viewed');
-	viewed.text(question.view_count);
-
-	// set some properties related to asker
-	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-													question.owner.display_name +
-												'</a>' +
-							'</p>' +
- 							'<p>Reputation: ' + question.owner.reputation + '</p>'
-	);
-
-	return result;
-};
-*/
-
 
 // this function takes the results object from StackOverflow
 // and creates info about search results to be appended to DOM
@@ -99,6 +119,41 @@ var showError = function(error){
 
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
+
+var getAnswerers = function(tag, period) {
+
+	var request = {tagged: tag,
+					site: 'stackoverflow',
+					order: 'desc',
+					sort: 'creation'};
+
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/" + tag + "/top-answerers/" + period,
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		console.log(result.items);
+		
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+		
+		$('.search-results').html(searchResults);
+		
+		$.each(result.items, function(i, item) {
+			var answer = showAnswerer(item);
+			$('.results').append(answer);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+
+	console.log(result);		
+
+};
+
 var getUnanswered = function(tags) {
 	
 	// the parameters we need to pass in our request to StackOverflow's API
